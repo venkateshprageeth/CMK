0d8a2724aa4242e99ede49148ae40d7fd5ea5856
root <root@cmk-dev.us-east1-b.c.devops-practice-282714.internal>
2020-07-21 13:13:23 +0000

    using regex to update main.mk file using hosts file

--- main.mk
+++ main.mk
@@ -1,8 +1,3 @@
-# Main configuration file of Check_MK
-#We highly recommend to use WATO to configure Check_MK these days.
-all_hosts = [
-"jenkins-master",
+all_hosts = [ "jenkins-master",
 "testing-check",
-"google-check",
-]
-
+"google-check", ]
