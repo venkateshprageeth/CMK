49057d3a6e12962d070f6d478103136b6d546ef8
root <root@puppet-master.venkat.com>
2020-07-22 05:43:30 +0000

    Updating hosts

--- main.mk
+++ main.mk
@@ -1,3 +1,9 @@
 all_hosts = [ "jenkins-master",
 "testing-check",
 "google-check", ]
+all_hosts = [ "puppet-master.venkat.com",
+"puppet-slave.venkat.com",
+"puppet-slave.venkat.com", ]
+all_hosts = [ "puppet-master.venkat.com",
+"ppet-slave1.venkat.com",
+"puppet-slave2.venkat.com", ]
